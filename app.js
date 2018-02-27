4e6b38509cc503ec7db2b0fd05674d5df38ca47a
Thom David <thomdavid74@gmail.com>
2018-02-27 08:36:50 -0800

    Set up MongoDB connection

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config'); // get the user config credentials
 
 var port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(__dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public')); // get static files
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // templating engine
+
+mongoose.connect(config.getDbConnectionString()); // connect to the mongo db
 
 app.listen(port);
